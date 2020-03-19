01ddd76538573840da448035265ef285f77b00dc
otaviobonassi <obonassi@gmail.com>
2020-03-19 15:18:10 -0300

    new version, with side scroll on the mobile version and optmized images

--- remkt.js
+++
@@ -1,7 +0,0 @@
-const body = document.querySelector('body');
-body.addEventListener('mouseout', popUp);
-
-function popUp() {
-    const h2 = document.querySelector('h2');
-    h2.classList.add('popup');
-}
